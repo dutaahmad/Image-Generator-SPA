a2daa69019c0be6626c3da2f23ce780ab54a54f8
dutaahmad <dutaahmadtefur@gmail.com>
2023-05-11 03:00:12 +0700

    Trial

---
+++ src/components/Loader.tsx
@@ -0,0 +1,13 @@
+export default function Loader(): React.JSX.Element {
+    return (
+        <div className="flex items-center justify-center p-5">
+
+            <div className="flex space-x-2 animate-pulse">
+                <div className="w-3 h-3 rounded-full bg-gray-50"></div>
+                <div className="w-3 h-3 rounded-full bg-gray-50"></div>
+                <div className="w-3 h-3 rounded-full bg-gray-50"></div>
+            </div>
+
+        </div>
+    )
+}
